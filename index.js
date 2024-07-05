27a7fd5fa3406c27d66893d8b355c4385cec8c5e
creinis <115181527+creinis@users.noreply.github.com>
2024-07-05 14:44:59 +0200

    cors test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const upload = multer()
 
 var app = express();
 
-/* app.use(cors()); */
+app.use(cors());
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
