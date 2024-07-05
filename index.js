9680967aae74bb9d4c9f17d753fd583752fcb8ad
creinis <115181527+creinis@users.noreply.github.com>
2024-07-05 14:29:28 +0200

    catch fs.stat

--- index.js
+++ index.js
@@ -9,18 +9,21 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 // Conexão com MongoDB
-mongoose.connect(process.env.MONGODB_URI, {})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connection to DB successful'))
-  .catch(err => console.log('MongoDB connection error:', err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Saída com erro se a conexão falhar
+  });
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
@@ -32,7 +35,7 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
   fs.stat(filePath, (err, stats) => {
     if (err) {
-      console.error(err);
+      console.error('File stat error:', err);
       return res.status(500).json({ error: 'Failed to retrieve file stats' });
     }
 
@@ -46,7 +49,7 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
     fs.unlink(filePath, (err) => {
       if (err) {
-        console.error(err);
+        console.error('File unlink error:', err);
       }
     });
 
