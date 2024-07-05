4d1a5b9a38c7709c1a363acb8e194b7d11103f0b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-05 14:45:35 +0200

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-/* var cors = require('cors'); */
+var cors = require('cors');
 require('dotenv').config()
 const multer = require('multer')
 const upload = multer()
